e89bafbd2a28c9d0d3de8c3697fadb95a6957b0f
Benjamin Schmidt <Benny@schmidtb.de>
2025-01-22 15:20:36 +0100

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Benjamin Schmidt on 22.01.25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
